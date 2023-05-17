64a790474d66a75806eec5d37c80d154767a92d1
John Taylor de Miranda Lobato <john13miranda@gmail.com>
2023-05-17 10:03:18 -0300

    novas perguntas adicionadas

--- src/app/pages/question1-a/question1-a.page.ts
+++ src/app/pages/question1-a/question1-a.page.ts
@@ -22,16 +22,88 @@ export class Question1APage implements OnInit {
       ]
     },
     {
-      title: "Quais são os tipos de hardware existentes em um sistema computacional",
+      title: "Quais são os tipos de hardware existentes em um sistema computacional?",
       answers: [
         { description: 'Hardware de saída, hardware de processamento e hardware de rede.', isRight: false },
         { description: 'Hardware de entrada, hardware de saída e hardware de armazenamento.', isRight: true },
         { description: ' Hardware de entrada, hardware de armazenamento e hardware de rede.', isRight: false },
         { description: 'Hardware de entrada, hardware de saída e hardware de processamento.', isRight: false }
       ]
+    },
+    {
+      title: "Qual é a função do hardware em um computador?",
+      answers: [
+        { description: 'Controlar o funcionamento do sistema operacional.', isRight: false },
+        { description: 'Executar programas e processar dados.', isRight: true },
+        { description: 'Armazenar informações e dados.', isRight: false },
+        { description: 'Estabelecer conexão com a internet.', isRight: false }
+      ]
+    },
+    {
+      title: "Qual é a importância da escolha de componentes de qualidade no hardware de um computador?",
+      answers: [
+        { description: 'Influenciar na velocidade de conexão com a internet.', isRight: false },
+        { description: 'Garantir a segurança dos dados armazenados no computador.', isRight: false },
+        { description: 'Melhorar a qualidade de som reproduzida pelo sistema de áudio.', isRight: false },
+        { description: 'Assegurar a eficiência e durabilidade do sistema computacional.', isRight: true }
+      ]
+    },
+    {
+      title: "Qual é a função do hardware de saída em um sistema computacional?",
+      answers: [
+        { description: 'Processar e armazenar dados.', isRight: false },
+        { description: 'Estabelecer conexão com a internet', isRight: false },
+        { description: 'Exibir informações na tela, reproduzir áudio e imprimir documentos.', isRight: true },
+        { description: 'Capturar dados do ambiente externo, como imagens e sons.', isRight: false }
+      ]
+    },
+    {
+      title: "Qual componente de hardware é responsável pelo gerenciamento e processamento de gráficos complexos em um computador?",
+      answers: [
+        { description: 'Placa de vídeo.', isRight: false },
+        { description: 'Placa de som.', isRight: false },
+        { description: 'Placa de rede.', isRight: true },
+        { description: 'Placa-mãe.', isRight: false }
+      ]
+    },
+    {
+      title: "Qual componente de hardware é responsável por fornecer energia elétrica estável e adequada para o funcionamento de todos os outros componentes de um computador?",
+      answers: [
+        { description: 'Processador.', isRight: false },
+        { description: 'Fonte de alimentação.', isRight: true },
+        { description: 'Placa de rede.', isRight: false },
+        { description: 'Disco rígido.', isRight: false }
+      ]
+    },
+    {
+      title: "Qual é o componente de hardware utilizado para exibir informações visuais em um computador?",
+      answers: [
+        { description: 'Mouse.', isRight: false },
+        { description: 'Teclado.', isRight: false },
+        { description: 'Monitor.', isRight: true },
+        { description: 'Impressora.', isRight: false }
+      ]
+    },
+    {
+      title: "Qual é o componente de hardware utilizado para inserir informações e comandos em um computador?",
+      answers: [
+        { description: 'Mouse.', isRight: false },
+        { description: 'Impressora.', isRight: false },
+        { description: 'Monitor.', isRight: false },
+        { description: 'Teclado.', isRight: true }
+      ]
+    },
+    {
+      title: "Qual é a função principal da memória RAM em um computador?",
+      answers: [
+        { description: 'Armazenar permanentemente os dados do usuário.', isRight: false },
+        { description: 'Realizar cálculos matemáticos complexos.', isRight: false },
+        { description: 'Fornecer energia elétrica aos demais componentes.', isRight: false },
+        { description: 'Armazenar temporariamente os dados em uso pelo sistema operacional e aplicativos.', isRight: true }
+      ]
     }
   ];
-  
+
   curQuestion!: Question;
   questionIndex: number = 0;
 
